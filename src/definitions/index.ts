1cad39f6b320a5b7b1f6111f95ccaab07c0cb2e2
ssorrrell <ssorrrell@hotmail.com>
2021-09-16 22:16:16 -0500

    differentiate completion keywords more

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -8,6 +8,10 @@ import { CompletionItem, CompletionItemKind } from "vscode";
 const LANGUAGE: string = "ecb2";
 
 const keywords = require(`./${getLocale()}/${LANGUAGE}/keywords.json`);
+const sound = require(`./${getLocale()}/${LANGUAGE}/sound.json`);
+const video = require(`./${getLocale()}/${LANGUAGE}/video.json`);
+const functions = require(`./${getLocale()}/${LANGUAGE}/functions.json`);
+const files = require(`./${getLocale()}/${LANGUAGE}/files.json`);
 const operators = require(`./${getLocale()}/${LANGUAGE}/operators.json`);
 
 const completions = new Array<CompletionItem>();
@@ -16,6 +20,34 @@ for (const entry in keywords) {
   const itm = new CompletionItem(entry, CompletionItemKind.Keyword);
   itm.detail = keywords[entry]?.documentation;
   //itm.documentation = keywords[entry]?.documentation;
+  completions.push(itm);
+}
+
+for (const entry in sound) {
+  const itm = new CompletionItem(entry, CompletionItemKind.Unit);
+  itm.detail = functions[entry]?.documentation;
+  //itm.documentation = functions[entry]?.documentation;
+  completions.push(itm);
+}
+
+for (const entry in video) {
+  const itm = new CompletionItem(entry, CompletionItemKind.Color);
+  itm.detail = functions[entry]?.documentation;
+  //itm.documentation = functions[entry]?.documentation;
+  completions.push(itm);
+}
+
+for (const entry in files) {
+  const itm = new CompletionItem(entry, CompletionItemKind.File);
+  itm.detail = functions[entry]?.documentation;
+  //itm.documentation = functions[entry]?.documentation;
+  completions.push(itm);
+}
+
+for (const entry in functions) {
+  const itm = new CompletionItem(entry, CompletionItemKind.Function);
+  itm.detail = functions[entry]?.documentation;
+  //itm.documentation = functions[entry]?.documentation;
   completions.push(itm);
 }
 
